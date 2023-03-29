f5413b784a78a8204dec1f2fda1b7c060d917d19
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-29 17:18:51 +0200

    add array with elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,39 @@ Bonus:
 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
 */
+
+const { createApp } = Vue
+
+createApp({
+   data() {
+      return {
+         activeImg: 0,
+         contentArray: {
+
+            images: [
+               "img/01.webp",
+               "img/02.webp",
+               "img/03.webp",
+               "img/04.webp",
+               "img/05.webp",
+             ],
+
+            titles: [
+              "Marvel's Spiderman Miles Morale",
+              "Ratchet & Clank: Rift Apart",
+              "Fortnite",
+              "Stray",
+              "Marvel's Avengers",
+            ],
+
+            descriptions: [
+              "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
+              "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
+              "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+              "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city.",
+              "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
+            ],
+          }
+      }
+   }
+}).mount('#app');
